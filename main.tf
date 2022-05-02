5cfeebbcd91756d1b35d60dc7763d0d726199aae
jamiewri <jamiewri@me.com>
2022-05-02 11:43:47 +1000

    fixing all the things

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "tfe_workspace" "workspace" {
   organization = var.organization
   name         = var.name
   description  = var.description
-  tag_names    = var.tags
+  tag_names    = var.tag_names
   auto_apply   = var.auto_apply
   vcs_repo {
-    identifier     = vcs_repo_identifier
-    branch         = vcs_repo_branch
-    oauth_token_id = vcs_repo_oauth_token_id
+    identifier     = var.vcs_repo_identifier
+    branch         = var.vcs_repo_branch
+    oauth_token_id = var.vcs_repo_oauth_token_id
   }
 }
 
